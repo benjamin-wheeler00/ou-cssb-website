6761d217533c63f2420364b06f10f1ed746336d3
Neb <NebulaSkies00@gmail.com>
2025-12-04 17:07:24 -0600

    Change data files to YAML

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 import { HtmlBasePlugin } from "@11ty/eleventy";
+import YAML from "yaml";
 
 export default async function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
@@ -10,6 +11,10 @@ export default async function (eleventyConfig) {
   eleventyConfig.ignores.add("src/**/.*");
   eleventyConfig.ignores.add("src/**/_*");
   eleventyConfig.ignores.add("src/**/*.11tydata.*");
+
+  eleventyConfig.addDataExtension("yml,yaml", (contents) =>
+    YAML.parse(contents),
+  );
 
   eleventyConfig.addPlugin(HtmlBasePlugin);
 }
